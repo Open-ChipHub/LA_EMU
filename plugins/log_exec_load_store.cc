4c8fae4a5cd3caf8002dccce56a78a7a565d1d67
Xinyu Li <lixinyu@loongson.cn>
2024-10-22 17:15:02 +0800

    plugin: example, log_exec_load_store.cc

---
+++ plugins/log_exec_load_store.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <assert.h>
+
+extern "C" {
+#include "../plugin.h"
+}
+
+
+static size_t haldle_gpr;
+
+static const char *const loongarch_r_alias[32] =
+{
+    "zer", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
+    "a4",   "a5", "a6", "a7", "t0", "t1", "t2", "t3",
+    "t4",   "t5", "t6", "t7", "t8", "r21","fp", "s0",
+    "s1",   "s2", "s3", "s4", "s5", "s6", "s7", "s8",
+};
+
+static void show_register(void *env) {
+    for (int i = 0; i <32; i++) {
+        printf("r%02d/%-3s:%016lx    ", i, loongarch_r_alias[i], la_emu_get_gpr(env, haldle_gpr, i));
+        if ((i + 1) % 4 == 0) {
+            printf("\n");
+        }
+    }
+}
+
+void my_emu_insn_before(void* env, uint64_t pc, uint32_t insn) {
+    printf("pc:%016lx insn:%09x\n", pc, insn);
+    show_register(env);
+}
+
+void print_data(char* dst, uint32_t size_shift, void* data) {
+    switch (size_shift) {
+        case 0: sprintf(dst, "%02x", *(uint8_t*)data);  break;
+        case 1: sprintf(dst, "%04x", *(uint16_t*)data);  break;
+        case 2: sprintf(dst, "%08x", *(uint32_t*)data);  break;
+        case 3: sprintf(dst, "%016lx", *(uint64_t*)data);  break;
+        case 4: sprintf(dst, "%016lx %016lx", *(uint64_t*)data, *((uint64_t*)data + 1));  break;
+        case 5: sprintf(dst, "%016lx %016lx %016lx %016lx", *(uint64_t*)data, *((uint64_t*)data + 1), *((uint64_t*)data + 2), *((uint64_t*)data + 3));  break;
+        default: assert(0);
+    }
+}
+
+void emu_load(uint64_t vaddr, uint32_t size_shift, void* data) {
+    char str_data[100];
+    print_data(str_data, size_shift, data);
+    fprintf(stderr, "load,vaddr,%016lx,data,%s\n", vaddr, str_data);
+}
+void emu_store(uint64_t vaddr, uint32_t size_shift, void* data) {
+    char str_data[100];
+    print_data(str_data, size_shift, data);
+    fprintf(stderr, "store,vaddr,%016lx,data,%s\n", vaddr, str_data);
+}
+
+la_emu_plugin_ops my_op = {
+    .emu_load = emu_load,
+    .emu_store = emu_store,
+};
+
+extern "C" la_emu_plugin_ops* la_emu_plugin_install(const char* arg) {
+    haldle_gpr = la_emu_get_handle_gpr();
+    printf("%lx\n", haldle_gpr);
+    return &my_op;
+}
